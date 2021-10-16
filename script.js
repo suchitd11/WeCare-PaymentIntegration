6e49e6ad5f66bf42e304f9802b3cb40f384a6f5e
suchitd11 <dsuchit111@gmail.com>
2021-10-16 12:23:56 +0530

    Co-authored-by: Prathamesh Gawas <Prathamesh010@users.noreply.github.com>

---
+++ script.js
@@ -0,0 +1,31 @@
+// function validateForm() {
+//     console.log("hello")
+//     let x = document.forms["myForm"]["name"].value;
+//     if (x == "") {
+//       alert("Name must be filled out");
+//       return false;
+//     }
+//     x = document.forms["myForm"]["email"].value;
+//     if (x == "") {
+//       alert("Email must be filled out");
+//       return false;
+//     }
+//     x = document.forms["myForm"]["date"].value;
+//     if (x == "") {
+//       alert("Date Must be selected");
+//       return false;
+//     }
+//     if (x == "12-11-2021") {
+//         alert("All Slots for the day are booked");
+//         return false;
+//     }
+//     x = document.forms["myForm"]["time"].value;
+//     if (x == "") {
+//         alert("Time Must be selected");
+//         return false;
+//     }
+//     if (x == "12:30") {
+//       alert("Slot for 12:30 is booked");
+//       return false;
+//     }
+//   }
